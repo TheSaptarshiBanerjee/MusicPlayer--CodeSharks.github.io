8cddc9913ec7791c174cbe1d9f7f60e37c4470ba
Saptarshi Banerjee <90561422+TheSaptarshiBanerjee@users.noreply.github.com>
2022-06-12 09:27:26 +0530

    my Music Player

---
+++ js/music-list.js
@@ -0,0 +1,59 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  
+  {
+    name: "Give Me Some Sunshine||3-idiots",
+    artist: "Suraj Jagan & Sharman Joshi",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "O zindagi yun gale aa lagi hai, aa lagi hai",
+    artist: "Arijit Singh",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Ratta Maar - Student Of The Year",
+    artist: "Vishal Dadlani,Shefali Alvarez",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Woh Din| Chhichhore |",
+    artist: "Pritam,Arijit Singh",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Ek Zindagi | Angrezi Medium |",
+    artist: "Tanishkaa Sanghvi, Sachin - Jigar",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Maine Tujhe Dekha Haste Hue",
+    artist: "Ali Zafar",
+    img: "music-12",
+    src: "music-12"
+  },
+
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
